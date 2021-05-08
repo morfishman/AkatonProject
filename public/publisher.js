40692fcf506b74f278b349ec743909870ba1f26f
morfishman <2023mor.p@ebin.ort.org.il>
2021-05-08 13:15:44 +0300

    Initial commit

---
+++ public/publisher.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const { title } = require('process')
+const  request  = require('request')
+const { error } = require('console')
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.get('',(req,res) =>{
+    res.render('mainPage.hbs',{
+        name: 'mor'
+    })
+})
+
+
+app.listen(port, () => {
+    console.log('server is ap on port ' + port)
+})
